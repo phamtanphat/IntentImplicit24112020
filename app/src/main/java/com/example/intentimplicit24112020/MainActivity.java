ebdf7d6c70133b400beb1f6a928c8e9e477bcd06
phatdroid94 <phatdroid94@gmail.com>
2021-01-30 18:30:52 +0700

    open camera

--- app/src/main/java/com/example/intentimplicit24112020/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit24112020/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.intentimplicit24112020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -43,8 +46,24 @@ public class MainActivity extends AppCompatActivity {
                                     new String[]{Manifest.permission.CAMERA},
                                     REQUEST_CODE_CAMERA
                             );
+                }else{
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent , REQUEST_CODE_CAMERA);
                 }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_CAMERA){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent , REQUEST_CODE_CAMERA);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
