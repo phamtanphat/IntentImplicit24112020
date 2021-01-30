b80d2cc72d1174f194d829101f728ba1929c5d97
phatdroid94 <phatdroid94@gmail.com>
2021-01-30 18:18:34 +0700

    check request camera

--- app/src/main/java/com/example/intentimplicit24112020/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit24112020/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.intentimplicit24112020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnCamera, mBtnGallery;
+    ImageView mImg;
+    int REQUEST_CODE_CAMERA = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnCamera = findViewById(R.id.buttonCamera);
+        mBtnGallery = findViewById(R.id.buttonGallery);
+        mImg = findViewById(R.id.imageview);
+
+        // sms : Hau nhu khong duoc dua len store
+        // contact : Duoc su dung it tren store
+
+        // GPS : dinh vi vi tri dang dung
+        // Open file pdf
+        // Chon nhieu hinh tu gallery
+        mBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat
+                            .requestPermissions(
+                                    MainActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    REQUEST_CODE_CAMERA
+                            );
+                }
+            }
+        });
     }
 }
