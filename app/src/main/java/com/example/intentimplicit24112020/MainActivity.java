f5cdec48835b67cf0a48595eeb2879e12f726a7b
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-02-02 18:31:08 +0700

    vao gallery

--- app/src/main/java/com/example/intentimplicit24112020/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit24112020/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     Button mBtnCamera, mBtnGallery;
     ImageView mImg;
     int REQUEST_CODE_CAMERA = 123;
+    int REQUEST_CODE_GALLERY = 234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +50,28 @@ public class MainActivity extends AppCompatActivity {
                                     new String[]{Manifest.permission.CAMERA},
                                     REQUEST_CODE_CAMERA
                             );
-                }else{
+                } else {
                     Intent intent = new Intent();
                     intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent , REQUEST_CODE_CAMERA);
+                    startActivityForResult(intent, REQUEST_CODE_CAMERA);
+                }
+            }
+        });
+        mBtnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_GALLERY
+                    );
+                } else {
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, REQUEST_CODE_GALLERY);
+
                 }
             }
         });
@@ -60,11 +79,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CODE_CAMERA){
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == REQUEST_CODE_CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Intent intent = new Intent();
                 intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent , REQUEST_CODE_CAMERA);
+                startActivityForResult(intent, REQUEST_CODE_CAMERA);
+            }
+        }
+        if (requestCode == REQUEST_CODE_GALLERY) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -72,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK){
+        if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK) {
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             mImg.setImageBitmap(bitmap);
         }
