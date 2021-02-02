a4bc83b6c113ed6ca276f97ec44d60142fd23fe3
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-02-02 18:41:29 +0700

    chuyển đổi stream sang bitmap

--- app/src/main/java/com/example/intentimplicit24112020/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit24112020/MainActivity.java
@@ -11,12 +11,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -102,6 +107,17 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK) {
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             mImg.setImageBitmap(bitmap);
+        }
+        if (requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK){
+//            Uri uri = data.getData();
+//            mImg.setImageURI(uri);
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                mImg.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
